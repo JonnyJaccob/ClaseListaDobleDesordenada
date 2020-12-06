553e2389ee92f9092a69c3201fc3ff82740f825f
JonnyJaccob <57542964+JonnyJaccob@users.noreply.github.com>
2020-12-05 18:51:09 -0600

    libreria 1.1

---
+++ ClassListaDoble/ClaseNodo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassListaDoble
+{
+    class ClaseNodo<Tipo>
+    {
+        public ClaseNodo()
+        {
+
+        }
+        Tipo _ObjetoConDatos;
+
+        public Tipo ObjetoConDatos
+        {
+            get { return _ObjetoConDatos; }
+            set { _ObjetoConDatos = value; }
+        }
+        private ClaseNodo<Tipo> _Siguente;
+
+        public ClaseNodo<Tipo> Siguiente
+        {
+            get { return _Siguente; }
+            set { _Siguente = value; }
+        }
+        private ClaseNodo<Tipo> _Previo;
+
+        public ClaseNodo<Tipo> Previo
+        {
+            get { return _Previo; }
+            set { _Previo = value; }
+        }
+        ~ClaseNodo()
+        {
+            ObjetoConDatos = default(Tipo);
+        }
+    }
+}
